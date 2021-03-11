56f7bf65a419fcd7946b003f3caca7395365d340
Yanxi <w1y2f3@126.com>
2021-03-11 13:19:11 -0800

    Upddate

---
+++ Week5-ASCII/Assets/Scripts/ASCIILoader.cs
@@ -0,0 +1,140 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class ASCIILoader : MonoBehaviour
+{
+//offset vars for the level position
+    public float xOffset;
+    public float yOffset;
+    
+    //Prefabs for the gameObjects we want to add to our scene
+    public GameObject player;
+    public GameObject wall;
+    public GameObject obstacle;
+    public GameObject black;
+    public GameObject red;
+    public GameObject orange;
+    public GameObject yellow;
+    public GameObject blue;
+    public GameObject green;
+    public GameObject purple;
+    public GameObject pink;
+    public GameObject navy;
+    public GameObject brown;
+    
+    
+    
+    //name of the level file
+    public string fileName = "level.txt";
+    
+
+    //empty game object that holds the level
+    public GameObject level;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        LoadLevel(); //call load level
+    }
+
+    //function that creates the level based on the ASCII Text File
+    void LoadLevel()
+    {
+        //build up a new level path based on the currentLevel
+        string current_file_path = //build path to the level file
+            Application.dataPath + 
+            "/Levels/" + 
+            fileName;
+
+        //pull the contents of the file into a string array
+        //each line in the file becomes an item in the array
+        string[] fileLines = File.ReadAllLines(current_file_path);
+        
+        //loop through each line
+        for (int y = 0; y < fileLines.Length; y++)
+        {
+            //get the current line
+            string lineText = fileLines[y];
+
+            //split the line into a char array
+            char[] characters = lineText.ToCharArray();
+
+            //loop through each char
+            for (int x = 0; x < characters.Length; x++)
+            {
+                //grab the current char
+                char c = characters[x];
+
+                //var for newObj
+                GameObject newObj;
+
+                switch (c) //switch statement for the car
+                {
+                    case 'p': //if char is a 'p'
+                        //make a player gameObject
+                        newObj = Instantiate<GameObject>(player);
+                        break;
+                    case 'w': //if char is a 'w'
+                        //make a wall
+                        newObj = Instantiate<GameObject>(wall);
+                        break;
+                    case '<':
+                        newObj = Instantiate<GameObject>(obstacle);
+                        break;
+                    case '!':
+                        newObj = Instantiate<GameObject>(black);
+                        break;
+                    case '@':
+                        newObj = Instantiate<GameObject>(red);
+                        break;
+                    case '#':
+                        newObj = Instantiate<GameObject>(orange);
+                        break;
+                    case '$':
+                        newObj = Instantiate<GameObject>(yellow);
+                        break;
+                    case '%':
+                        newObj = Instantiate<GameObject>(blue);
+                        break;
+                    case '^':
+                        newObj = Instantiate<GameObject>(green);
+                        break;
+                    case '&':
+                        newObj = Instantiate<GameObject>(purple);
+                        break;
+                    case '*':
+                        newObj = Instantiate<GameObject>(pink);
+                        break;
+                    case '(':
+                        newObj = Instantiate<GameObject>(navy);
+                        break;
+                    case ')':
+                        newObj = Instantiate<GameObject>(brown);
+                        break;
+                    
+                    default: //if the char is anything else
+                        newObj = null;  //make newObj null
+                        break;
+                }
+                if (newObj != null) //if newObj is not null
+                {
+                   
+                    //whatever newObj is, set it's position based on the offsets
+                    //and it's position in the file
+                    newObj.transform.position = 
+                        new Vector3(x + xOffset, -y + yOffset, 0);
+                }
+
+              
+            }
+        }
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Week5-ASCII/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,124 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    public float forceAmount = 5;  //public var for force amount
+    
+    Rigidbody2D rb2D; //var for the Rigidbody2D
+
+    //static variable means the value is the same for all the objects of this class type and the class itself
+    public static PlayerControl instance; //this static var will hold the Singleton
+
+    public GameObject black1;
+    public GameObject red1;
+    public GameObject orange1;
+    public GameObject yellow1;
+    public GameObject blue1;
+    public GameObject green1;
+    public GameObject purple1;
+    public GameObject pink1;
+    public GameObject navy1;
+    public GameObject brown1;
+
+    private GameObject obj = null;
+
+    private Vector2 colorPositon = new Vector2(2, 2);
+
+    private int random = 0;
+    void Awake()
+    {
+        if (instance == null)  //instance hasn't been set yet
+        {
+            DontDestroyOnLoad(gameObject);  //Dont Destroy this object when you load a new scene
+            instance = this; //set instance to this object
+        }
+        else //if the instance is already set to an object
+        {
+            Destroy(gameObject);  //destroy this new object, so there is only ever one
+        }
+    }
+
+    void ChangeColor()
+    {
+        switch (random)
+        {
+            case '0':
+                obj = Instantiate<GameObject>(black1);
+                break;
+            case '1':
+                obj = Instantiate<GameObject>(red1);
+                break;
+            case '2':
+                obj = Instantiate<GameObject>(orange1);
+                break;
+            case '3':
+                obj = Instantiate<GameObject>(yellow1);
+                break;
+            case '4':
+                obj = Instantiate<GameObject>(blue1);
+                break;
+            case '5':
+                obj = Instantiate<GameObject>(green1);
+                break;
+            case '6':
+                obj = Instantiate<GameObject>(purple1);
+                break;
+            case '7':
+                obj = Instantiate<GameObject>(pink1);
+                break;
+            case '8':
+                obj = Instantiate<GameObject>(navy1);
+                break;
+            case '9':
+                obj = Instantiate<GameObject>(brown1);
+                break;
+            default:
+                obj = null;
+                break;
+        }
+
+      
+        
+            obj.transform.position = colorPositon;
+        
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>(); //get the Rigidbody2D  off of this gameObject
+        ChangeColor();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.W)) //if W is pressed
+        {
+            rb2D.AddForce(Vector2.up * forceAmount); //apply to the up mult by the "force" var
+            random = Random.Range(0, 9);
+            print(random);
+            ChangeColor();
+        }
+
+        if (Input.GetKey(KeyCode.S)) //if S is pressed
+        {
+            rb2D.AddForce(Vector2.down * forceAmount); //apply to the up mult by the "force" var
+        }
+
+        if (Input.GetKey(KeyCode.A)) //if A is pressed
+        {
+            rb2D.AddForce(Vector2.left * forceAmount); //apply to the up mult by the "force" var
+        }
+
+        if (Input.GetKey(KeyCode.D)) //if D is pressed
+        {
+            rb2D.AddForce(Vector2.right * forceAmount); //apply to the up mult by the "force" var
+        }
+        
+        
+    }
+}
